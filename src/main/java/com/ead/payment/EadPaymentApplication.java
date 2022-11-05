75f41a0ca5c49f4699e06130877a505faa7dedb4
Mario Miranda <mario_chan15@hotmail.com>
2022-11-05 13:43:26 -0400

    payment service basic structure project

---
+++ src/main/java/com/ead/payment/EadPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.ead.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EadPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EadPaymentApplication.class, args);
+    }
+
+}
